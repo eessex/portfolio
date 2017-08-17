557e6664256229d367bb471e19506c90132fb68e
Eve Essex <eve.essex@gmail.com>
2017-08-16 22:54:26 -0400

    update build process

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'test') {
   app.use(logger('dev')); //TODO- combined for prod
 }
 
-mongoose.connect('mongodb://localhost/' + db);
+mongoose.connect(db);
 
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");

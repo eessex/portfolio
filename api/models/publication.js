5360f8c59c46df5644ad2772bec504a2cf8079bc
Eve Essex <eve.essex@gmail.com>
2018-01-01 17:19:07 -0500

    modify publication schema

--- api/models/publication.js
+++ api/models/publication.js
@@ -8,7 +8,17 @@ var PublicationSchema = new Schema({
   release_date: String,
   publisher: String,
   format: String,
+  formats: [
+    {
+      publisher: { type: String },
+      format: { type: String },
+      release_year: { type: Number },
+      compilation: { type: Boolean, default: false },
+      featuring: { type: Boolean, default: false }
+    }
+  ],
   embed_url: String,
+  embed_urls: { type: Array, default: [] },
   description: String,
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },

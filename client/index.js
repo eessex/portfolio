eb7ade3671b8b64ab39169c498d4c0e832a8fb51
Eve Essex <eve.essex@gmail.com>
2018-01-07 00:07:19 -0500

    move store to utils

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import Routes from './routes'
-import store from './store'
+import store from './utils/store.js'
 import { saveState } from './utils/local_storage.js'
 import { fetchSettings } from './actions/settings'
 require('./styles/index.scss')

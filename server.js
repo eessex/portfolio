359167eb56829712d8f7c8da7a2caa982fe2d227
Eve Essex <eve.essex@gmail.com>
2018-05-20 15:34:39 -0400

    upgrade yarn version, mongo settings

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'test') {
   app.use(logger('dev'))
 }
 
-mongoose.connect(db)
+mongoose.connect(db, { useMongoClient: true })
 
 if (process.env.NODE_ENV === 'production') {
   app.use(function(req, res, next) {

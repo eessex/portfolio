2f6a3c96c66ff4c68d2e41da6646017725967021
Eve Essex <eve.essex@gmail.com>
2018-05-28 16:44:23 -0400

    ImageShow tests

---
+++ client/components/image/__tests__/image_show.test.js
@@ -0,0 +1,32 @@
+import { mount } from 'enzyme'
+import React from 'react'
+import { ImageShow } from '../image_show'
+
+describe('ImageShow', () => {
+  const getWrapper = props => {
+    return mount(
+      <ImageShow {...props} />
+    )
+  }
+
+  let props = {
+    aspect: 1,
+    caption: '<p>A caption</p>',
+    url: '/image.jpg'
+  }
+
+  it('Renders image', () => {
+    const component = getWrapper(props)
+    expect(component.find('img').getElement().props.src).toBe(props.url)
+  })
+
+  it('Renders caption', () => {
+    const component = getWrapper(props)
+    expect(component.html()).toMatch(props.caption)
+  })
+
+  it('Renders caption as image alt', () => {
+    const component = getWrapper(props)
+    expect(component.html()).toMatch('alt="A caption"')
+  })
+})

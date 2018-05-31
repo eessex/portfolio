14126aa9f3bcd1971f5f9680b77be3c8113c95db
Eve Essex <eve.essex@gmail.com>
2018-05-31 19:05:46 -0400

    cleanup settings

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,7 @@
 const asyncActionType = (type) => ({
-    PENDING: `${type}_PENDING`,
-    SUCCESS: `${type}_SUCCESS`,
-    ERROR: `${type}_ERROR`,
+  PENDING: `${type}_PENDING`,
+  SUCCESS: `${type}_SUCCESS`,
+  ERROR: `${type}_ERROR`
 })
 
 export const API = 'API'

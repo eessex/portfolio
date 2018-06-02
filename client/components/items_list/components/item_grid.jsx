c255ac93a1351b54802efa2c62c65a62d64dda8b
Eve Essex <eve.essex@gmail.com>
2018-06-02 16:37:39 -0400

    convert items-list to styled components

--- client/components/items_list/components/item_grid.jsx
+++ client/components/items_list/components/item_grid.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { Formats } from '../../formats/formats.jsx'
@@ -5,6 +6,7 @@ import { Formats } from '../../formats/formats.jsx'
 export const ItemGrid = props => {
   const {
     artist,
+    condensed,
     date,
     description,
     formats,
@@ -16,42 +18,82 @@ export const ItemGrid = props => {
   const titleClass = hasImage ? ' h3' : ' h1'
 
   return (
-    <div className='Item ItemGrid'>
+    <GridItem condensed={condensed}>
       {hasImage &&
         <img
-          className='Item__img'
           src={image.url}
-          width='100%' />
+          width='100%'
+        />
       }
-      <div className='Item__body'>
-        <div className={'Item__title' + titleClass}>
+      <div>
+        <div className={titleClass}>
           {artist && `${artist}: `}
           {title || 'Missing Title'}
         </div>
         {date && !formats &&
-          <h5 className='Item__date'>
+          <h5>
             {date}
           </h5>
         }
         {venue &&
-          <h5 className='Item__venue'>
+          <h5>
             {venue}
           </h5>
         }
         {formats && formats.length &&
-          <h5 className='Item__formats'>
+          <h5>
             <Formats items={formats} />
           </h5>
         }
         {description &&
-          <p className='Item__description'>
+          <p>
             {description}
           </p>
         }
       </div>
-    </div>
+    </GridItem>
   )
 }
+
+const GridItem = styled.div`
+  margin-bottom: 4em;
+  padding: 10px 0;
+  .h1 {
+    font-size: 4.25em;
+  }
+  img {
+    max-height: 80vh;
+    width: auto;
+    max-width: 100%;
+  }
+
+  ${props => props.condensed === true && `
+    margin-top: 30px;
+    margin-bottom: 50px;
+    padding: 0 10px;
+    .h1 {
+      font-size: 2.5em;
+    }
+    .h3 {
+      margin-bottom: .5em;
+    }
+    @media (max-width: 76rem) {
+      margin-top: 10px;
+      margin-bottom: 30px;
+      padding: 10px 20px;
+    }
+  `}
+
+  @media (max-width: 76rem) {
+    .h1 {
+      font-size: 3.25em;
+    }
+    h4 {
+      font-size: 1.3em;
+      line-height: 1.3em;
+    }
+  }
+`
 
 ItemGrid.propTypes = {
   artist: PropTypes.string,

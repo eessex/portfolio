d1ba071a62d87019609ac6b849d0001498017684
Eve Essex <eve.essex@gmail.com>
2018-11-07 23:45:30 -0500

    page model supports embeds

--- src/api/apps/pages/schema.js
+++ src/api/apps/pages/schema.js
@@ -14,18 +14,10 @@ var PageSchema = new Schema({
     unique: true
   },
   images: { type: Array, default: [] },
+  embed_codes: [],
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
   published: { type: Boolean, default: false }
-})
-
-PageSchema.pre('save', next => {
-  const now = new Date()
-  if (!this.created_at) {
-    this.created_at = now
-  }
-  this.updated_at = now
-  next()
 })
 
 module.exports = mongoose.model('Page', PageSchema)

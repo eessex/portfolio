7d34403b02ed720684362b5894e7a914591b9da2
Eve Essex <eve.essex@gmail.com>
2019-06-08 15:42:58 -0400

    button is ts

--- src/client/components/Button/__tests__/index.test.tsx
+++ src/client/components/Button/__tests__/index.test.tsx
@@ -5,7 +5,10 @@ import { Button } from '../index'
 
 describe('Button', () => {
   const child = <FileInput onChange={jest.fn()} fetchUpload={jest.fn()} />
-  let props = {}
+  let props
+  const getWrapper = (passedProps = props) => {
+    return mount(<Button {...passedProps} />)
+  }
 
   beforeEach(() => {
     props = {
@@ -15,32 +18,28 @@ describe('Button', () => {
   })
 
   it('renders a button', () => {
-    const component = mount(
-      <Button {...props} />
-    )
+    const component = getWrapper()
     expect(component.find('button').length).toBe(1)
     expect(component.text()).toMatch(props.text)
   })
 
   it('renders an icon if props.icon', () => {
     props.icon = 'times'
-    const component = mount(
-      <Button {...props} />
-    )
+    const component = getWrapper()
     expect(component.html()).toMatch('fa-times')
   })
 
   it('renders a child', () => {
     const component = mount(
-      <Button {...props}>{child}</Button>
+      <Button {...props}>
+        {child}
+      </Button>
     )
     expect(component.find(FileInput).length).toBe(1)
   })
 
   it('Calls props.onClick when clicked', () => {
-    const component = mount(
-      <Button {...props} />
-    )
+    const component = getWrapper()
     const button = component.find('button')
     button.simulate('click')
     expect(props.onClick.mock.calls.length).toBe(1)
